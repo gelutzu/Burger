9b214eed940d64430fe516c5cde5b034118321bf
gelutzu <gelutzu@yahoo.com>
2018-03-16 15:33:08 -0500

    working version

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big', true);
+INSERT INTO burgers (burger_name) VALUES ('Bigger');
+INSERT INTO burgers (burger_name) VALUES ('Biggest');
+INSERT INTO burgers (burger_name) VALUES ('Huge');
+INSERT INTO burgers (burger_name) VALUES ('DonaldHuge');
+INSERT INTO burgers (burger_name) VALUES ('RocketMan');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-state").on("click", function(event) {
+        var id = $(this).data("id");
+        var newState = $(this).data("newstate");
+
+        var newBurgerState = {
+            devoured: newState
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newBurgerState
+        }).then(
+            function() {
+                console.log("changed status to", newState);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#brg").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
+});
